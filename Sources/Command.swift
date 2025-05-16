1d86b66779502b72205554292598ceada6f6edc7
Håkon Harnes <hakon@harnes.co>
2025-05-16 17:17:58 +0200

    feat: add version flag

--- Sources/Command.swift
+++ Sources/Command.swift
@@ -1,10 +1,19 @@
 import ArgumentParser
 import Foundation
 
+let version = """
+pbctl 0.1.0
+Copyright (C) 2025 Håkon Harnes
+License MIT <https://opensource.org/licenses/MIT>
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+"""
+
 @main
 struct Pbctl: ParsableCommand {
     static let configuration = CommandConfiguration(
         abstract: "Command-line interface to the MacOS pasteboard",
+        version: version,
         subcommands: [
             Copy.self,
             Paste.self,
