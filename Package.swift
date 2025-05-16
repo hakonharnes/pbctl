45d87b055ec3c55bcd8e24294739b24207df5e4b
Håkon Harnes <hakon@harnes.co>
2025-05-16 16:02:06 +0200

    chore: initial

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 6.1
+
+import PackageDescription
+
+let package = Package(
+    name: "pbctl",
+    platforms: [
+        .macOS(.v11),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/apple/swift-argument-parser.git",
+            from: "1.5.0"
+        ),
+        .package(
+            url: "https://github.com/hakonharnes/CLibmagic.git",
+            from: "0.7.2"
+        ),
+    ],
+
+    targets: [
+        .executableTarget(
+            name: "pbctl",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "MagicWrapper", package: "CLibmagic"),
+            ]
+        ),
+    ]
+)
